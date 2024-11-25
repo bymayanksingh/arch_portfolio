1757c0c2a09c370602e6a9cf2c9d781f4ccca3ef
Mayank Singh <mayank.singh081997@gmail.com>
2024-11-25 13:07:42 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
       {/* Background Image with Overlay */}
       <div className="absolute inset-0">
         <img 
-          src="https://images.unsplash.com/photo-1487958449943-2429e8be8625?auto=format&fit=crop&q=80&w=2000"
+          src="https://images.unsplash.com/photo-1637088059531-4ffcc89d0dd3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Abstract Architecture"
           className="w-full h-full object-cover"
         />
