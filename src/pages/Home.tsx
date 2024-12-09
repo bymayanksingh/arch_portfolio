a7a8d908e957de2b96479fe321d74b92acf97bdc
Mayank Singh <mayank.singh081997@gmail.com>
2024-12-10 00:02:35 +0530

    Add graduation videos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import { Hero } from '../components/Hero';
 import { HomeAbout } from '../components/HomeAbout';
 import { Timeline } from '../components/Timeline';
 import { Projects } from '../components/Projects';
 import { Testimonials } from '../components/Testimonials';
+import { GraduationVideo } from '../components/GraduationVideo';
 
 export function Home() {
   return (
     <>
       <Hero />
       <HomeAbout />
+      <GraduationVideo />
       <Timeline />
       <Projects />
       <Testimonials />
